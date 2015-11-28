30cdac49d9c0e124a22361287869c2578b366365
Kacper Wikiel <random8902@gmail.com>
2015-11-28 21:53:25 +0100

    WIP parsing loans

--- getloans.py
+++ getloans.py
@@ -1,10 +1,10 @@
 # Parse loans into loans database
-
+import datetime
 import requests
 from models import Loan
 from models import db
 
-
+DATAFORMAT  =  "%Y-%m-%dT%H:%M:%SZ"
 def loan_byid(lid):
     raw = requests.get("https://api.loanbase.com/api/loan/"+str(lid))
     pl = raw.json()['loans'][0]
@@ -13,7 +13,12 @@ def loan_byid(lid):
         type=pl['type'],
         description=pl['description'],
         amount=float(pl['amount']),
-        term=int(pl['amount'])
+        term=int(pl['term']),
+        frequency = int(pl['frequency']),
+        status = pl['status'],
+        paymentStatus = pl['paymentStatus'],
+        createdAt = datetime.datetime.strptime(pl['createdAt'],DATAFORMAT).date()
+        expirationDate = datetime.datetime.strptime(pl['expirationDate'],DATAFORMAT).date()
     )
     db.session.add(l)
     db.session.commit()
