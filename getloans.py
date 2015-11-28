b2e56478842d333a05c490cea45fe54695e22868
Kacper Wikiel <random8902@gmail.com>
2015-11-28 23:51:24 +0100

    Better parsing dates

--- getloans.py
+++ getloans.py
@@ -7,11 +7,13 @@ from models import db
 import logging
 
 logger = logging.getLogger(__name__)
-
+logging.basicConfig(level=logging.DEBUG)
 
 def parse_date(datestr):
-    return datetime.datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%SZ').date()
-
+    try:
+        datetime.datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%SZ').date()
+    except:
+        datetime.datetime.strptime(datestr[:-10], '%Y-%m-%dT%H:%M:%S').date()
 
 def loan_byid(lid):
     try:
