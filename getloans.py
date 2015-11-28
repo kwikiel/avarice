79cda2a579bd9e0d44628a3e6c979dd49e09724d
Kacper Wikiel <random8902@gmail.com>
2015-11-28 22:17:10 +0100

    PEP8 cleanup

--- getloans.py
+++ getloans.py
@@ -4,7 +4,9 @@ import requests
 from models import Loan
 from models import db
 
-DATAFORMAT  =  "%Y-%m-%dT%H:%M:%SZ"
+DATAFORMAT = "%Y-%m-%dT%H:%M:%SZ"
+
+
 def loan_byid(lid):
     raw = requests.get("https://api.loanbase.com/api/loan/"+str(lid))
     pl = raw.json()['loans'][0]
@@ -14,11 +16,18 @@ def loan_byid(lid):
         description=pl['description'],
         amount=float(pl['amount']),
         term=int(pl['term']),
-        frequency = int(pl['frequency']),
-        status = pl['status'],
-        paymentStatus = pl['paymentStatus'],
-        createdAt = datetime.datetime.strptime(pl['createdAt'],DATAFORMAT).date()
-        expirationDate = datetime.datetime.strptime(pl['expirationDate'],DATAFORMAT).date()
-    )
+        frequency=int(pl['frequency']),
+        status=pl['status'],
+        paymentStatus=pl['paymentStatus'],
+        createdAt=datetime.datetime.strptime(pl['createdAt'], DATAFORMAT)
+        .date(),
+        expirationDate=datetime.datetime.strptime(pl['expirationDate'], DATAFORMAT)
+        .date(),
+        denomination=pl['denomination'],
+        percentFunded=float(pl['percentFunded']),
+        votes=int(pl['votes']),
+        borrower=int(pl['borrower']),
+        creditScore=pl['creditScore'])
+
     db.session.add(l)
     db.session.commit()
