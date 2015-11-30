702b7285575cd84645880e2028a3cc00478dcac5
Kacper Wikiel <random8902@gmail.com>
2015-11-30 18:37:40 +0100

    PEP8 and dependencies

---
+++ getinvestments.py
@@ -0,0 +1,36 @@
+import requests
+import datetime
+
+from models import Investment
+from models import db
+
+
+def parse_date(datestr):
+    '''
+    Helper function for parsing dates
+    '''
+    try:
+        datetime.datetime.strptime(datestr, '%Y-%m-%d %H:%M:%S').date()
+    except:
+        datetime.datetime.strptime(datestr[:-10], '%Y-%m-%dT%H:%M:%S').date()
+
+
+def investment_byloan(lid):
+    '''
+    Get investment by loan ID
+    '''
+    r = requests.get("https://api.loanbase.com/api/investments"+str(lid))
+    for i in r.json()['investments']:
+        inv = Investment(
+            id=int(i['id']),
+            amount=float(i['amount']),
+            dateInvested=parse_date(i['dateInvested']),
+            rate=float(i['rate']),
+            loanId=int(i['loanId']),
+            investorId=int(i['investorId']))
+        try:
+            db.session.add(inv)
+            db.session.commit()
+        except IOError:
+            db.session.rollback()
+            print "Rollbacked"
