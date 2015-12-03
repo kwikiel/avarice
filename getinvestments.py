a34e49d38b1e93c6c956f2979cffc501536d9c7e
Kacper Wikiel <random8902@gmail.com>
2015-12-03 12:00:16 +0100

    Checking emptyness

--- getinvestments.py
+++ getinvestments.py
@@ -19,17 +19,24 @@ def investment_byloan(lid):
     '''
     Get investment by loan ID
     '''
-    r = requests.get("https://api.loanbase.com/api/investments/"+str(lid))
-    for i in r.json()['investments']:
-        inv = Investment(
-            id=int(i['id']),
-            amount=float(i['amount']),
-            dateInvested=parse_date(i['dateInvested']),
-            rate=float(i['rate']),
-            loanId=int(i['loanId']),
-            investorId=int(i['investorId']))
+    try:
+        r = requests.get("https://api.loanbase.com/api/investments/"+str(lid))
+    except:
+        print("Cannot get data from API")
+    if r.json()['investments']:
         try:
-            db.session.add(inv)
-            db.session.commit()
+                for i in r.json()['investments']:
+                    inv = Investment(
+                        id=int(i['id']),
+                        amount=float(i['amount']),
+                        dateInvested=parse_date(i['dateInvested']),
+                        rate=float(i['rate']),
+                        loanId=int(i['loanId']),
+                        investorId=int(i['investorId']))
+                    try:
+                        db.session.add(inv)
+                        db.session.commit()
+                    except:
+                        db.session.rollback()
         except:
-            db.session.rollback()
+            print("Prolly empty {0}".format(lid))
